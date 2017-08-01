e73694c5bf528a2aeec79cc14ebf5a4de85bf51c
Kevin Ernst <kevin.ernst@cchmc.org>
2017-08-01 16:04:40 -0400

    Add .gitattributes; "script" -> "src"

--- example/Example_run.bat
+++ example/Example_run.bat
@@ -1,15 +1,15 @@
-../script/RELI \
--snp SLE_EU.snp  \
--ld SLE_EU.ld \
--index ../data/ChIPseq.index \
--data ../data/ChIP-seq \
--target hg19_0302 \
--build ../data/GenomeBuild/hg19.txt \
--null ../data/Null/CommonSNP_MAFmatch \
--dbsnp ../data/SNPtable/SNPtable \
--out Output   \
--match \
--rep 2000 \
--corr 1544 \
--phenotype Systemic_Lupus_Erythematosus \
--ancestry EU
+../src/RELI \
+  -snp SLE_EU.snp  \
+  -ld SLE_EU.ld \
+  -index ../data/ChIPseq.index \
+  -data ../data/ChIP-seq \
+  -target hg19_0302 \
+  -build ../data/GenomeBuild/hg19.txt \
+  -null ../data/Null/CommonSNP_MAFmatch \
+  -dbsnp ../data/SNPtable/SNPtable \
+  -out Output   \
+  -match \
+  -rep 2000 \
+  -corr 1544 \
+  -phenotype Systemic_Lupus_Erythematosus \
+  -ancestry EU
--- script/compile.bat
+++
@@ -1,3 +0,0 @@
-g++ RELI.cpp RELI_impl.h  RELI_impl.cpp  \
--o RELI -std=c++11 -O3 -w \
--lgsl -lgslcblas 
